bc6bbd0c59868e90dd87c03b4a838f930aa30ebb
Magnus Poppe Wang <Magnus.Poppe.Wang@gmail.com>
2016-02-25 11:55:07 +0100

    Lagt til kommentarer i top.

--- Oblig4/Mandelbrot/Point.java
+++ Oblig4/Mandelbrot/Point.java
@@ -3,6 +3,10 @@ package Oblig4.Mandelbrot;
 import javafx.scene.paint.Color;
 
 /**
+ * Klassen brukes til å holde på fargen når den går
+ * igjennom programmet. Brukes med "Mandelbrot"
+ * klassen.
+ *      - Magnus
  * Created by jooivind on 25.02.2016.
  */
 public class Point implements Scalable {
--- Oblig4/Mandelbrot/Scalable.java
+++ Oblig4/Mandelbrot/Scalable.java
@@ -1,6 +1,8 @@
 package Oblig4.Mandelbrot;
 
 /**
+ * Kravspec for alle komplekse tall.
+ *
  * Created by jooivind on 25.02.2016.
  */
 public interface Scalable {
